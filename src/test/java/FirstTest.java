43a374dd498c49dfe5383a9013aed0c44911cad1
tatarynov <tatarynov@MacBook-Pro.local>
2017-07-25 19:20:28 +0200

    added desiredCapabilities

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,8 +1,34 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.remote.MobilePlatform;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+
+import java.io.File;
 import java.net.MalformedURLException;
+import java.net.URL;
+
 
 public class FirstTest {
 
     public static void main(String[] args) throws MalformedURLException {
+        File classPathRoot = new File(System.getProperty("user.dir"));
+        File appDir = new File(classPathRoot, "/app/Android");
+        File app = new File(appDir, "Contacts.apk");
+
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, MobilePlatform.ANDROID);
+        desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Any Device Name");
+        desiredCapabilities.setCapability(MobileCapabilityType.APP, app);
+        desiredCapabilities.setCapability("appPackage", "com.jayway.contacts");
+        desiredCapabilities.setCapability("appActivity", "com.jayway.contacts.MainActivity");
+        desiredCapabilities.setCapability("newCommandTimeout", 300);
+
+        AppiumDriver<MobileElement> driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), desiredCapabilities);
+        driver.getPlatformName();
     }
 }
 
+// CTRL/CMD + ALT + L  - formatting code
